4dd57ef11217835a44e8635140c8df2e0e71f208
isaac mwesigwa <mwesigwai433@gmail.com>
2023-10-10 21:06:45 +0300

    Another commit

---
+++ solidPractice/AddBooks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solidPractice
+{
+    public class AddBooks
+    {
+         public void AddBook(string author, string title)
+         {
+            Book bookToAdd = new(author, title);
+
+            if (Library.bookCount < Library.catalog.Length)
+            {
+                Library.catalog[Library.bookCount] = bookToAdd;
+                Library.bookCount++;
+            }
+         }
+    }
+}
---
+++ solidPractice/DeleteBooks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solidPractice
+{
+    public class DeleteBooks
+    {
+        public void deleteBook(string author, string title)
+        {
+            int indexToRemove = -1;
+            Book bookToRemove = new(author, title);
+            for (int i = 0; i < Library.bookCount; i++)
+            {
+                if (Library.catalog[i].title == bookToRemove.title && Library.catalog[i].author == bookToRemove.author)
+                {
+                    indexToRemove = i;
+                    break;
+                }
+            }
+            for (int i = indexToRemove; i < Library.catalog.Length - 1; i++)
+            {
+                Library.catalog[i] = Library.catalog[i + 1];
+                Library.bookCount--;
+            }
+        }
+    }
+}
